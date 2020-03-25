544a5cb0a51314db3dd0821d8e5b6df16be5bd03
James <Jiawu1998@github.com>
2020-03-25 17:01:59 -0500

    Counting sort complete

--- CountingSort.java
+++ CountingSort.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CountingSort{
+    // COUNTING SORT RESTRICTION: CAN ONLY SORT FROM 0 to N 
+
     public static Integer[] array; 
     public ArrayList<ArrayList<Integer>> snap_shots = new ArrayList<ArrayList<Integer>>();
 
@@ -25,5 +27,39 @@ public class CountingSort{
         snap_shots = new ArrayList<ArrayList<Integer>>();
     }
 
+    public void sort(){
+        Integer[] temp = array;
     
+        int max_range = 0;
+        for(int i=0; i<array.length; i++){
+            if (array[i] > max_range){
+                max_range = array[i];
+            }
+            if (array[i] < 0){
+                System.out.print("Counting Sort Restriction: You cannot have a negative number.");
+                return;
+            }            
+        }
+
+        Integer[] index = new Integer[max_range];
+
+        for(int i=0; i<index.length; i++){
+            index[i] = 0;
+        } 
+
+        for(int i=0; i<array.length; i++){
+            index[array[i]] += 1;
+        }
+
+        for(int i=1; i<index.length; i++){
+            index[i] += index[i-1];
+        }
+
+        for(int i=0; i<array.length; i++){
+            temp[index[array[i]]] = array[i];
+            index[array[i]] -= 1;
+        }
+
+        array = temp;
+    }
 }
